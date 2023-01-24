64ca21509105c94caa790eebb3d2e37ee86fb66b
Ryan Belcher <ryanmbelcher86@gmail.com>
2023-01-24 18:58:31 -0500

    added resume.js

---
+++ src/components/pages/Resume.js
@@ -0,0 +1,28 @@
+export default function Resume() {
+    return (
+        <div>
+            <h1>Resume</h1>
+            <p>Download my resume</p>
+            <h1>Front-end Proficiencies</h1>
+            <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JavaScript</li>
+                <li>JQeury</li>
+                <li>Responsive Design</li>
+                <li>React</li>
+                <li>Bootstrap</li>
+            </ul>
+            <h1>Back-end Proficiencies</h1>
+            <ul>
+                <li>APIs</li>
+                <li>Node</li>
+                <li>Express</li>
+                <li>MySQL, Sequelize</li>
+                <li>MonoDB, Mongoose</li>
+                <li>REST</li>
+                <li>GraphQL</li>
+            </ul>
+        </div>
+    )
+}
