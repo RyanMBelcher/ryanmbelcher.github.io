03df6d26027cf1dfe30ad02af8384c9baea3ac47
Ryan Belcher <ryanmbelcher86@gmail.com>
2023-01-24 17:55:38 -0500

    started header, navigation and navitem components

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import { NavItem } from './NavItem';
+import { Navigation } from './Navigation';
+
+export function Header() {
+    return (
+        <header>
+            <Navigation />
+        </header>
+    );
+};
