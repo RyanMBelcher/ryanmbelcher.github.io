84c0b719b37ddf7c479a5fbf8161a3788833e125
Ryan Belcher <ryanmbelcher86@gmail.com>
2023-01-24 20:28:43 -0500

    added email regex

---
+++ src/components/pages/Contact.js
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+
+export function Contact() {
+
+
+    return (
+        <div>
+            <h1>Contact</h1>
+            <form>
+                <input />
+                <input />
+                <input />
+            </form>
+        </div>
+    )
+}
