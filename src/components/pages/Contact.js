0390fd0bb76ad9c4a50247a003612c7ce401d4d0
Ryan Belcher <ryanmbelcher86@gmail.com>
2023-01-24 21:03:56 -0500

    created contact form

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,16 +1,81 @@
 import { useState } from 'react';
+import { validateEmail } from '../../utils/helpers';
 
-export function Contact() {
+export default function Contact() {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
 
+    const handleInputChange = (e) => {
+        const { target } = e;
+        const inputType = target.name;
+        const inputValue = target.value;
+
+        if (inputType === 'name') {
+            setName(inputValue);
+        } else if (inputType === 'email') {
+            setEmail(inputValue)
+        } else {
+            setMessage(inputValue)
+        }
+    };
+
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+
+        if (!name) {
+            setErrorMessage('Name is required');
+            return;
+        }
+
+        if (!validateEmail(email)) {
+            setErrorMessage('Your email is invalid');
+            return;
+        }
+
+        if (!message) {
+            setErrorMessage('Message is required');
+            return;
+        }
+
+        setName('');
+        setEmail('');
+        setMessage('');
+    };
 
     return (
         <div>
             <h1>Contact</h1>
             <form>
-                <input />
-                <input />
-                <input />
+                <input
+                    value={name}
+                    name='name'
+                    onChange={handleInputChange}
+                    type='text'
+                    placeholder='Name'
+                />
+                <input
+                    value={email}
+                    name='email'
+                    onChange={handleInputChange}
+                    type='email'
+                    placeholder='Email'
+                />
+                <input
+                    value={message}
+                    name='message'
+                    onChange={handleInputChange}
+                    type='text'
+                    placeholder='Message'
+                />
+                <button type='button' onClick={handleFormSubmit}>Submit</button>
             </form>
+            {errorMessage && (
+                <div>
+                    <p>{errorMessage}</p>
+                </div>
+            )}
         </div>
-    )
+    );
 }
