fedbe8d640fbbb7eee8b9e841f10fbd525c1c955
Ryan Belcher <ryanmbelcher86@gmail.com>
2023-02-15 15:33:46 -0500

    add decolines

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { Footer } from './components/Footer';
 function App() {
   return (
     <div className="App">
+      <div id="decoLinesContainer" />
       <PortfolioContainer />
       <Footer />
     </div>
