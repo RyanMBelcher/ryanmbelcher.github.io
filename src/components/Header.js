e19f6612ae714c85f4088ca35f1b89af09167cc6
Ryan Belcher <ryanmbelcher86@gmail.com>
2023-04-19 15:19:56 -0400

    about me section responsive layout.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { NavItem } from './NavItem';
+import Hamburger from './Hamburger';
 import '../styles/Header.css'
+import '../styles/Hamburger.css'
 
 export function Header({ currentPage, handlePageChange }) {
+    const [hamburgerOpen, setHamburgerOpen] = useState(false);
+
+    const toggleHamburger = () => {
+        setHamburgerOpen(!hamburgerOpen);
+    }
+
+    // const isMobile = kljekljre;
+
     return (
-        <>
+        <div className='navigation'>
             <header>
                 <ul className='header-list'>
                     <NavItem content='About Me' onClick={() => handlePageChange('AboutMe')} page='AboutMe' currentPage={currentPage} />
@@ -12,9 +22,22 @@ export function Header({ currentPage, handlePageChange }) {
                     <NavItem content='Contact' onClick={() => handlePageChange('Contact')} page='Contact' currentPage={currentPage} />
                     <NavItem content='Resume' onClick={() => handlePageChange('Resume')} page='Resume' currentPage={currentPage} />
                 </ul>
+                <div className='hamburger' onClick={toggleHamburger}>
+                    <Hamburger />
+                </div>
+                <style jsx>
+                { `
+                    .navigation ul{
+                        display: ${hamburgerOpen ? 'inline' : 'none'};
+                        height: 100vh;
+                        width: 50vw;
+                        margin-top: 50px;
+                        position: absolute;
+                    }
+                    `}</style>
             </header>
 
 
-        </>
+        </div>
     );
 };
